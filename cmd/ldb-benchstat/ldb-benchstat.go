e0142efee0d953e752a85dfb44bf5a0d56caed34
rjl493456442 <garyrong0905@gmail.com>
2020-07-14 11:24:40 +0800

    cmd/ldb-benchstat: fix mean and stdev speeds
    Since bytes/sec is a 'speed', the duration must be taken into account
    when computing the mean. Example: the mean speed of 10 B/s for 2s then
    5 B/s for 10s is 5.8 B/s. Disregarding the durations would calculate
    an incorrect mean speed of 7.5 B/s.

--- cmd/ldb-benchstat/ldb-benchstat.go
+++ cmd/ldb-benchstat/ldb-benchstat.go
@@ -15,15 +15,17 @@ func main() {
 	for _, r := range reports {
 		var (
 			bps       []float64
+			weight    []float64
 			totalTime float64
 			totalSize uint64
 		)
 		for _, ev := range r.Events {
 			bps = append(bps, ev.BPS())
+			weight = append(weight, float64(ev.Duration))
 			totalTime += float64(ev.Duration) / float64(time.Second)
 			totalSize += ev.Delta
 		}
-		meanBPS, stdBPS := stat.MeanStdDev(bps, nil)
+		meanBPS, stdBPS := stat.MeanStdDev(bps, weight)
 		fmt.Printf("-- %s (%d events)", r.Name, len(r.Events))
 		fmt.Printf(" total time: %.4fs\n", totalTime)
 		fmt.Printf(" total size: %d bytes\n", totalSize)
